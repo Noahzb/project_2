3904c7867a78046b8087d61bb53d451e3680c731
smoot <smoothtacomann@gmail.com>
2018-12-06 20:39:57 -0500

    INPUT

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
@@ -11,6 +12,8 @@ module.exports = function(app) {
     });
   });
 
+
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -18,6 +21,10 @@ module.exports = function(app) {
         example: dbExample
       });
     });
+  });
+
+  app.get("/input", function(req, res) {
+    res.render(path.join(__dirname, "../views/input.handlebars"));
   });
 
   // Render 404 page for any unmatched routes
