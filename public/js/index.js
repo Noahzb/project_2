38237b4ea7a92c932db12ac2fac285ae0d8def29
smoot <smoothtacomann@gmail.com>
2018-12-08 10:49:48 -0500

    do not merge just copy paste

--- public/js/index.js
+++ public/js/index.js
@@ -40,7 +40,7 @@ var refreshExamples = function() {
   API.getExamples().then(function(data) {
     var $examples = data.map(function(example) {
       var $a = $("<a>")
-        .text(example.text)
+        .text(example.text + " to " + example.description)
         .attr("href", "/example/" + example.id);
 
       var $li = $("<li>")
